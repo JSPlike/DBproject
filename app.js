ed92fcc49f6cc9f2bb50ebbc48fb0bbba0f902a5
JoonYoung <moter2588@gmail.com>
2017-11-22 14:23:31 +0900

    app.js파일, jade, qury

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-var express = require('express');
 
+//선언부분
+var express = require('express');
 var app = express();
 
+//jade파일 형식을 보기좋게
+app.locals.pretty = true;
+
+//템플릿 엔진사용
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+
+//정적 파일(미들웨어)
+//public directory로 지정
+app.use(express.static('public'));
+
+
+//views 라우터 부분
+//express api 원리 알기
+app.get('/template', function(req, res){
+    res.render('index', {time:Date(), _title:'Jade'});
+});
+
+//라우터 부분(TEST)
 app.get('/', function(req, res){
-    res.send('Hello JoonYoung');
+    res.send('<h1>Hello JoonYoung</h1>');
 
 });
 
@@ -11,8 +32,9 @@ app.get('/login', function(req, res){
     res.send('Login please');
 });
 
+
+//연결 완료 메시지
 app.listen(3000, function(){
     console.log('Connected 3000 port!!')
 
 });
-
